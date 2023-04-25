236c391c287dac460225c549d8a6d96b49b14753
nikolayilin <n.ilin@it-park.tech>
2023-04-25 21:13:49 +0300

    nw

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), unique=True)
     password = db.Column(db.String(128))
